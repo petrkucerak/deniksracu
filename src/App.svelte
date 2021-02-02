650c5920fc4bbe83b8f69a888cdf0fbf83e9d02b
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-02-02 17:09:50 +0100

    add leaflet docs for offline work in train

--- src/App.svelte
+++ src/App.svelte
@@ -37,7 +37,7 @@
 				attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
 	        &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
 				subdomains: "abcd",
-				maxZoom: 14,
+				maxZoom: 20,
 			}
 		).addTo(m);
 
