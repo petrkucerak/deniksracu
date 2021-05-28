7b396e7d41ff27816e77e69595099fa593030633
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-28 19:55:05 +0200

    comment calling functions & import geolocation file

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,60 @@
 <script>
    // import app variables
 
-	import L from "leaflet";
+   import L from "leaflet";
+   import getCords from "Geolocation";
 
    // define global varibales
 
-	let map;
+   let map;
 
-	// inti view Prague
-	const initialView = [50.0755, 14.4378];
-	function createMap(container) {
-		let m = L.map(container, { preferCanvas: true }).setView(initialView, 14);
-		L.tileLayer(
-			"https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
-			{
-				attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+   // inti view Prague
+   const initialView = [50.0755, 14.4378];
+   function createMap(container) {
+      let m = L.map(container, { preferCanvas: true }).setView(initialView, 14);
+      L.tileLayer(
+         "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+         {
+            attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
 		  &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
-				subdomains: "abcd",
-				maxZoom: 20,
-			}
-		).addTo(m);
+            subdomains: "abcd",
+            maxZoom: 20,
+         }
+      ).addTo(m);
 
-		return m;
-	}
+      return m;
+   }
 
-	function mapAction(container) {
-		map = createMap(container);
+   /**
+    * Function is connect with div container with map
+    * @param container
+    */
+   function mapAction(container) {
+      map = createMap(container);
 
-		return {
-			destroy: () => {
-				map.remove();
-				map = null;
-			},
-		};
-	}
+      return {
+         destroy: () => {
+            map.remove();
+            map = null;
+         },
+      };
+   }
 
-	function resizeMap() {
-		if (map) {
-			map.invalidateSize();
-		}
-	}
-
+   /**
+    * Function run if user resize windows
+    */
+   function resizeMap() {
+      if (map) {
+         map.invalidateSize();
+      }
+   }
 </script>
 
 <svelte:window on:resize={resizeMap} /><link
-	rel="stylesheet"
-	href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-	integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-	crossorigin=""
+   rel="stylesheet"
+   href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+   crossorigin=""
 />
 
 <div class="map" use:mapAction />
