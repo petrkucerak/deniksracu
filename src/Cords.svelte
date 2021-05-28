fcbbfa21c00311d95cbec433093a393977ed9a46
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-05-28 22:15:04 +0200

    print user coords by using watchPosition

--- src/Cords.svelte
+++ src/Cords.svelte
@@ -1,5 +1,37 @@
 <script context="module">
+   import { error } from "console";
+
+   // define global varibales
+   let id, cordOptions;
+
+   cordOptions = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+   };
+
+   /**
+    * Function to get user cords
+    */
    export function getCords() {
-      console.log("test message");
+      console.log("User cords are");
+
+      // create id vaiable to watching user location
+      id = navigator.geolocation.watchPosition(
+         cordSucess,
+         cordError,
+         cordOptions
+      );
+
+      function cordSucess(pos) {
+         let crd = pos.coords;
+
+         console.log("Latitude: " + crd.latitude);
+         console.log("Longitude: " + crd.longitude);
+      }
+
+      function cordError(err) {
+         console.warn("ERROR(" + err.code + "): " + err.message);
+      }
    }
 </script>
