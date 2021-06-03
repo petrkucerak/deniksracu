3a4f218eda4909a73efd6529ab03278d715c1c55
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-06-03 20:01:16 +0200

    import get user position

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
    // import app variables
 
    import L from "leaflet";
+   import { placeUserPosition } from "./UserPosition.js";
 
    // define global varibales
 
@@ -30,7 +31,8 @@
     */
    function mapAction(container) {
       map = createMap(container);
-      placeUserPosition();
+
+      placeUserPosition(map);
 
       return {
          destroy: () => {
@@ -47,43 +49,6 @@
       if (map) {
          map.invalidateSize();
       }
-   }
-
-   function placeUserPosition() {
-      // placeholders for the L.marker and L.circle representing user's current position and accuracy
-      var current_position, current_accuracy;
-
-      function onLocationFound(e) {
-         // if position defined, then remove the existing position marker and accuracy circle from the map
-         if (current_position) {
-            map.removeLayer(current_position);
-            map.removeLayer(current_accuracy);
-         }
-
-         var radius = e.accuracy / 2;
-
-         current_position = L.marker(e.latlng)
-            .addTo(map)
-            .bindPopup("You are within " + radius + " meters from this point")
-            .openPopup();
-
-         current_accuracy = L.circle(e.latlng, radius).addTo(map);
-      }
-
-      function onLocationError(e) {
-         alert(e.message);
-      }
-
-      map.on("locationfound", onLocationFound);
-      map.on("locationerror", onLocationError);
-
-      // wrap map.locate in a function
-      function locate() {
-         map.locate({ setView: true, maxZoom: 16 });
-      }
-
-      // call locate every 3 seconds... forever
-      setInterval(locate, 3000);
    }
 </script>
 
