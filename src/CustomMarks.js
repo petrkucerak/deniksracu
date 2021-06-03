a5b4c504c2996f149a71746551907b7130394d8b
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-06-04 00:15:04 +0200

    optimalize images

--- src/CustomMarks.js
+++ src/CustomMarks.js
@@ -2,11 +2,11 @@
  * Custom icon for user position
  */
 export let userIcon = L.icon({
-   iconUrl: './img/cowboy.png',
+   iconUrl: './img/cowboy.webp',
    iconSize: [38, 38] // size of the icon
 });
 
 export let toiletIcon = L.icon({
-   iconUrl: './img/toilet.png',
+   iconUrl: './img/toilet-small.webp',
    iconSize: [30, 30]
 });
