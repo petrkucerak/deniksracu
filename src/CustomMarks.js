f8e30c6304faab8384eb41f31862e8f0461e6761
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-06-04 00:15:27 +0200

    add comment

--- src/CustomMarks.js
+++ src/CustomMarks.js
@@ -6,6 +6,9 @@ export let userIcon = L.icon({
    iconSize: [38, 38] // size of the icon
 });
 
+/**
+ * Custom icon for toilet positions.
+ */
 export let toiletIcon = L.icon({
    iconUrl: './img/toilet-small.webp',
    iconSize: [30, 30]
