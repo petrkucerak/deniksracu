5d6b7ebfeb001ad0026e8a671dc99c96f141cec0
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-06-03 22:49:17 +0200

    remove unused user position sttings

--- src/CustomMarks.js
+++ src/CustomMarks.js
@@ -5,7 +5,4 @@ export let userIcon = L.icon({
    iconUrl: './img/cowboy.png',
 
    iconSize: [38, 38], // size of the icon
-   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-   shadowAnchor: [4, 62],  // the same for the shadow
-   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
