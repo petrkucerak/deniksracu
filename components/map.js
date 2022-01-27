7896a51fbe1553094a0dbff83a42e45b479a5cfb
Petr Kučera <petr@khome.cz>
2022-01-27 20:35:56 +0000

    Watch srac position.

--- components/map.js
+++ components/map.js
@@ -1,12 +1,15 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet/dist/leaflet.css";
+import { sracLocation } from "../lib/location/srac-location";
 
 export default function Map() {
+  const { latitude, longitude, error } = sracLocation();
+  console.log(latitude, longitude, error);
   return (
     <MapContainer
-      center={[50.0806100, 14.4101822]}
+      center={[50.08061, 14.4101822]}
       zoom={20}
       scrollWheelZoom={false}
       className="w-screen h-screen"
@@ -15,6 +18,7 @@ export default function Map() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      
     </MapContainer>
   );
 }
