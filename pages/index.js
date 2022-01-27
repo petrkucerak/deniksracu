38c79c735a105d40b5e425a7e548a9681ba2438f
Petr Kučera <petr@khome.cz>
2022-01-27 18:13:21 +0100

    Transafer2Next (#29)
    * Remove old files.
    
    * Create next.js app template.
    
    * Create page about rebuild.
    
    * Update project config.
    
    * Install tailwind
    
    * Add repo vscode extensions

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Deník sráčů</title>
+      </Head>
+      <main>
+        <h1 className="text-2xl">Deník sráčů</h1>
+        <p className="">Stránka je momentálně v přestavbě.</p>
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
