4bb6798c8f1638da204c9cee086511486cef7418
Petr Kučera <petr@khome.cz>
2022-01-28 10:25:19 +0000

    Simply footer style

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <footer>
+        <footer className="flex flex-col items-center py-3 bg-gray-100">
             <p>petr@khome.cz</p>
             <p>repo projektu</p>
         </footer>
