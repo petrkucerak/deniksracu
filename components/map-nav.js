2fd8c2c9ca224a11452cfd37c396d7e4abc4157f
Petr Kučera <petr@khome.cz>
2022-01-28 23:55:51 +0000

    simply custom nav menu

--- components/map-nav.js
+++ components/map-nav.js
@@ -1,16 +1,37 @@
 import { X, Menu } from "react-feather";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function MapNav() {
+    const [closeClass, setCloseClass] = useState("hidden");
+    const [openClass, setOpenClass] = useState("z-[10001] fixed top-4 right-4 p-1 bg-white rounded border border-gray-600");
+    const [navClass, setNavClass] = useState("hidden");
+    let isOpen = false;
+
+    function switchMenuContext() {
+        console.log(isOpen);
+        if (isOpen) {
+            setCloseClass("hidden");
+            setOpenClass("z-[10001] fixed top-4 right-4 p-1 bg-white rounded border border-gray-600");
+            setNavClass("hidden");
+            isOpen = false;
+        } else {
+            setCloseClass("z-[10001] fixed top-4 right-4 p-1 bg-white rounded border border-white");
+            setOpenClass("hidden");
+            setNavClass("flex z-[10000] flex-col fixed top-0 left-0 w-screen h-screen bg-white text-lg uppercase font-semibold items-center text-center");
+            isOpen = true;
+        }
+    }
+
     return (
         <>
-            <button className="z-[10001] fixed top-4 right-4 p-1 bg-white rounded border border-white">
+            <button className={closeClass} onClick={() => switchMenuContext()}>
                 <X />
             </button>
-            <button className="z-[10001] fixed top-4 right-4 p-1 bg-white rounded border border-gray-600">
+            <button className={openClass} onClick={() => switchMenuContext()}>
                 <Menu />
             </button>
-            <nav className="z-[10000] flex flex-col fixed top-0 left-0 w-screen h-screen bg-white text-lg uppercase font-semibold items-center text-center">
+            <nav className={navClass}>
                 <h1 className="mt-5 font-bold text-2xl normal-case border-b-2 border-gray-600">Deník sráčů</h1>
                 <Link href="/form">
                     <a id="home" className="mt-5">Přidat nový trůn</a>
