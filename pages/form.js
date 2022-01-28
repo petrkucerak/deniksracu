578173a7c2bcd38a6018bf8024095c82cc3872eb
Petr Kučera <petr@khome.cz>
2022-01-28 00:12:51 +0000

    Create the page for form.

---
+++ pages/form.js
@@ -0,0 +1,17 @@
+import Meta from "../components/meta";
+
+export default function Home() {
+  return (
+    <div>
+      <Meta />
+      <main className="p-8">
+        <h1 className="text-4xl">Přidat nový záchod</h1>
+        <section>
+
+        </section>
+        <button className="">nahrát</button>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
