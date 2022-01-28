28379713ea653fc6d88f2d1c0c52655ae9481219
Petr Kučera <petr@khome.cz>
2022-01-28 13:23:36 +0000

    Shared states

--- components/form-toggle-question.js
+++ components/form-toggle-question.js
@@ -1,11 +1,14 @@
 import ToggleSwitch from "./toggle-switch";
+import { useState } from "react";
 
 export default function FromToggleQuestion({ title, description }) {
+    const [sharedState, setSharedState] = useState(false);
+    console.log(sharedState);
     return (
         <div className="mb-2">
             <p className="font-semibold">{title}</p>
             <p>{description}</p>
-            <ToggleSwitch />
+            <ToggleSwitch sharedState={sharedState} setSharedState={setSharedState}/>
         </div>
     )
 }
--- components/toggle-switch.js
+++ components/toggle-switch.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 
-function ToggleSwitch() {
+function ToggleSwitch({ sharedState, setSharedState }) {
   const [isToggled, setIsToggled] = useState(false);
-  const onToggle = () => setIsToggled(!isToggled);
+  const onToggle = () => {
+    setIsToggled(!isToggled);
+    setSharedState(isToggled);
+  };
   return (
     <label className="toggle-switch my-2">
       <input type="checkbox" checked={isToggled} onChange={onToggle} />
