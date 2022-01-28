1e0fe70e2d40ff6ed874633676120d6d08542900
Petr Kučera <petr@khome.cz>
2022-01-28 21:44:23 +0000

    Add burger nav menu

--- components/map-nav.js
+++ components/map-nav.js
@@ -1,6 +1,11 @@
+import { Menu } from "react-feather";
+
 export default function MapNav() {
     return (
-        <>
-        </>
+        <nav className="absolute top-5 right-5 z-[500]">
+            <button className="rounded bg-white p-1 border-2 border-gray-500">
+                <Menu />
+            </button>
+        </nav>
     );
 }
