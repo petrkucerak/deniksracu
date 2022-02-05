7b76c2cf08f2119ee493270fc39a20c506813cac
Petr Kučera <petr@khome.cz>
2022-02-05 20:29:34 +0000

    Can read the body

--- worker/form.js
+++ worker/form.js
@@ -1,28 +1,28 @@
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request));
-})
-
-// ToDo need to be specified
-const corsHeaders = {
+ })
+ 
+ // ToDo need to be specified
+ const corsHeaders = {
     "Access-Control-Allow-Headers": "*",
     "Access-Control-Allow-Methods": "POST",
     "Access-Control-Allow-Origin": "*"
-}
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
+ }
+ /**
+  * Respond with hello worker text
+  * @param {Request} request
+  */
+ async function handleRequest(request) {
     if (request.method === "OPTIONS") {
-        return new Response("OK", { headers: corsHeaders });
+       return new Response("OK", { headers: corsHeaders });
     }
-    if (request.method === "POST") {
-        return new Response(JSON.stringify(request.body), {
-            headers: {
-                "Content-type": "appliaction/json",
-                ...corsHeaders
-            }
-        });
-    }
-
-}
+    const body = await request.json();
+    return new Response(JSON.stringify(body), {
+       headers: {
+          "Content-type": "appliaction/json",
+          ...corsHeaders
+       }
+    });
+ 
+ }
+ 
