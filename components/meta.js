b114b75c747525e0400a6ca7ec0ca47f527cf285
petrkucerak <petr@khome.cz>
2022-05-12 23:04:10 +0200

    Create manifest & add favicons and icons

--- components/meta.js
+++ components/meta.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
 
 export default function Meta() {
+  const description = "Jsi na procházce a potřebuješ na velkou? Nezoufej, deník sráčů tě zachrání!";
+  const title = "Deník sráčů";
+  const keywords = "Deník sráčů, záchod, toalety, WC, položit kabel, saulit, trůnění, sráč";
+  const url = "https://deniksracu.cz/";
   return (
     <Head>
       <title>Deník sráčů</title>
@@ -8,6 +12,74 @@ export default function Meta() {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.76.0/dist/L.Control.Locate.min.css"
       />
+
+      <meta name="application-name" content={title} />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="PWA App" />
+      <meta
+        name="description"
+        content={description}
+      />
+      <meta name="keywords" content={keywords} />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#fbbf24" />
+
+      <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/favicon/touch-icon-ipad.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/touch-icon-iphone-retina.png" />
+      <link rel="apple-touch-icon" sizes="167x167" href="/favicon/touch-icon-ipad-retina.png" />
+
+      <link href="splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+      <link href="splashscreens/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/manifest.json" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content={url}/>
+      <meta name="twitter:title" content={title} />
+      <meta
+        name="twitter:description"
+        content={description}
+      />
+      <meta name="twitter:site" content="@deniksracu" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta
+        property="og:description"
+        content={description}
+      />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:url" content={url} />
+
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#000000" />
+
+      <meta name="robots" content="all" />
+
     </Head>
   );
 }
