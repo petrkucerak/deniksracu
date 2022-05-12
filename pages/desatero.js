583009f6c9e3566070530028b9e4e6294cb0dd77
petrkucerak <petr@khome.cz>
2022-05-12 20:35:44 +0200

    Add desatero

--- pages/desatero.js
+++ pages/desatero.js
@@ -3,6 +3,7 @@ import Footer from "../components/footer";
 import Section from "../components/section";
 import SectionHeader from "../components/section-header";
 import SectionBody from "../components/section-body";
+import LocalTitle from "../components/local-title";
 
 export default function Desatero() {
 
@@ -13,8 +14,22 @@ export default function Desatero() {
         Sráčova desatero
       </h1>
       <Section>
-        <SectionHeader title="přikázání"/>
+        <SectionHeader title="přikázání" />
         <SectionBody>
+          <LocalTitle title="Prolog" subtitle="Jsou dny, kdy se všechno otáčí jako dobře promazaný držák na toaletní papír. Těmito dny proplouváš bez většího problému. Pak se ale stane, že ani v klidu nevysereš. Nestíháš, jsi rozhádaný, zkolí tě nemoc, nepovedla se ti zkouška a tvoje oáza už není klidnou. Právě pro tyto dny slouží desatero, které ti ukáže cestu skrz temný tunel. Díky němu se neztratíš a budeš se držet základních zásad, které z tebe dělají správného sráče." />
+          <LocalTitle title="Přikázání" subtitle="" />
+          <ol className="list-decimal ml-8">
+            <li className="mb-2">Do jednoho záchodu sráti budeš!</li>
+            <li className="my-2">Během sraní nebudeš záchody střídati!</li>
+            <li className="my-2">Pomni, abys záchod spláchnul i v den sváteční!</li>
+            <li className="my-2">Budeš ctít ostatní sráče!</li>
+            <li className="my-2">Po potřebě splachovati budeš!</li>
+            <li className="my-2">Nebudeš nakukovati do kabinky vedlejší!</li>
+            <li className="my-2">Nebudeš krást papír toaletní!</li>
+            <li className="my-2">Budeš hodnotit vždy pravdivě a objektivně!</li>
+            <li className="my-2">Nebudeš žádostivě dychtit po roličce toaletního papíru svého bližního!</li>
+            <li className="mt-2">Aniž požádáš štětky jeho!</li>
+          </ol>
         </SectionBody>
       </Section>
       <Footer />
