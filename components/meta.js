db3734831098a15758238f15af8c997b7c12a17a
Petr Kučera <petr@khome.cz>
2022-10-21 13:40:43 +0200

    Favicon (#82)
    * Remove favicons
    
    * Remove favicons from meta
    
    * Generate icons for PWA
    
    * Remove unused meta
    
    * Remove a pwa favicon generator package
    
    * Optimised images with calibre/image-actions
    
    * Remove a pwa favicon generator package
    
    * Add favion
    
    * Optimised images with calibre/image-actions
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/meta.js
+++ components/meta.js
@@ -6,6 +6,7 @@ export default function Meta() {
   const title = "Deník sráčů";
   const keywords =
     "Deník sráčů, záchod, toalety, WC, položit kabel, saulit, trůnění, sráč";
+  const mainColor = `af8566ff`;
   const url = "https://deniksracu.cz/";
   return (
     <Head>
@@ -16,6 +17,32 @@ export default function Meta() {
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
       />
 
+      <link rel="manifest" href="/manifest.json" />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="favicon/favicon-16x16.png"
+      />
+      <link
+        rel="mask-icon"
+        href="favicon/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+
       <meta name="application-name" content={title} />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -25,206 +52,7 @@ export default function Meta() {
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="msapplication-tap-highlight" content="no" />
-      <meta name="theme-color" content="#fbbf24" />
-
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="public/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="public/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="public/favicon/site.webmanifest" />
-
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="196x196"
-        href="public/favicon/favicon-196.png"
-      />
-
-      <meta
-        name="msapplication-square70x70logo"
-        content="public/favicon/mstile-icon-128.png"
-      />
-      <meta
-        name="msapplication-square150x150logo"
-        content="public/favicon/mstile-icon-270.png"
-      />
-      <meta
-        name="msapplication-square310x310logo"
-        content="public/favicon/mstile-icon-558.png"
-      />
-      <meta
-        name="msapplication-wide310x150logo"
-        content="public/favicon/mstile-icon-558-270.png"
-      />
-
-      <link rel="apple-touch-icon" href="public/favicon/apple-icon-180.png" />
-
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2048-2732.jpg"
-        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2732-2048.jpg"
-        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1668-2388.jpg"
-        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2388-1668.jpg"
-        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1536-2048.jpg"
-        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2048-1536.jpg"
-        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1668-2224.jpg"
-        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2224-1668.jpg"
-        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1620-2160.jpg"
-        media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2160-1620.jpg"
-        media="(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1290-2796.jpg"
-        media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2796-1290.jpg"
-        media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1179-2556.jpg"
-        media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2556-1179.jpg"
-        media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1284-2778.jpg"
-        media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2778-1284.jpg"
-        media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1170-2532.jpg"
-        media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2532-1170.jpg"
-        media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1125-2436.jpg"
-        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2436-1125.jpg"
-        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1242-2688.jpg"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2688-1242.jpg"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-828-1792.jpg"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1792-828.jpg"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1242-2208.jpg"
-        media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-2208-1242.jpg"
-        media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-750-1334.jpg"
-        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1334-750.jpg"
-        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-640-1136.jpg"
-        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-      />
-      <link
-        rel="apple-touch-startup-image"
-        href="public/favicon/apple-splash-1136-640.jpg"
-        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-      />
-      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content={mainColor} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:url" content={url} />
@@ -237,9 +65,6 @@ export default function Meta() {
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={title} />
       <meta property="og:url" content={url} />
-
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
 
       <meta name="robots" content="all" />
     </Head>
