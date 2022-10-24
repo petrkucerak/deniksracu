a0ac3247540b8b46ca5a1794a75cdbac57d34287
petrkucerak <petr@khome.cz>
2022-10-24 14:53:26 +0200

    Define structure for a new marker

--- components/map-toilets.js
+++ components/map-toilets.js
@@ -10,10 +10,22 @@ export default function MapToilets() {
     .then((toilets) => {
       for (let id = 0; id < toilets.length; id += 1) {
         console.log(toilets[id]);
-        L.marker([toilets[id].latitude, toilets[id].longtitude]).addTo(map);
+        L.marker([toilets[id].latitude, toilets[id].longtitude], {
+          icon: toiletIcon,
+        }).addTo(map);
       }
     })
     .catch((err) => console.log(err));
 
   return null;
 }
+
+const toiletIcon = L.icon({
+  iconUrl: "my-icon.png",
+  iconSize: [38, 95],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowUrl: "my-icon-shadow.png",
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94],
+});
