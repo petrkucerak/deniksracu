fe1c5b4950b621570e7ed838ecb93d334fcc4c4b
petrkucerak <petr@khome.cz>
2022-10-24 14:06:51 +0200

    Remove async functions

--- components/map-toilets.js
+++ components/map-toilets.js
@@ -2,10 +2,10 @@ import { useMap } from "react-leaflet";
 
 const url = `public/toilets/toilets-unsorted.json`;
 
-export default async function MapToilets() {
+export default function MapToilets() {
   const map = useMap();
-  const res = await fetch(url);
-  const toilets = await res.json();
+  const res = fetch(url);
+  const toilets = res.json();
   console.log(toilets);
 
   return null;
