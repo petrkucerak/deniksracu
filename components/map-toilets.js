6353f797387b7117f9e71cbaeced0fa945e7e6d3
petrkucerak <petr@khome.cz>
2022-10-24 14:27:13 +0200

    Add json with toilets data

--- components/map-toilets.js
+++ components/map-toilets.js
@@ -5,6 +5,13 @@ const url = `public/toilets/toilets-unsorted.json`;
 export default function MapToilets() {
   const map = useMap();
 
+  fetch(url)
+    .then((res) => res.json())
+    .then((toilets) => {
+      console.log(toilets);
+    })
+    .catch((err) => console.log(err));
+
   console.log("Map functions!");
 
   return null;
