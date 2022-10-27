cdd3f66fd0a0dcf4aeb63bbef7bd46895fdbf7f9
petrkucerak <petr@khome.cz>
2022-10-27 23:45:51 +0200

    Add icon

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,21 @@
+import { IconBrandGithub, IconMail } from "@tabler/icons";
 import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center py-3 bg-gray-200 text-gray-800">
-      <Link href="mailto:petr@khome.cz">
-        <a className="underline">petr@khome.cz</a>
+    <footer className="flex flex-col items-center py-5 bg-gray-200 text-gray-800">
+      <Link href="https://github.com/petrkucerak/deniksracu">
+        <a className="w-[90vw]" target="_blank" rel="noreferrer">
+          <IconBrandGithub className="inline mr-2" />
+          deniksracu
+        </a>
       </Link>
-      <p>
-        Chceš se pomoci deníku sráčů, mrkni do{" "}
-        <Link href="https://github.com/petrkucerak/deniksracu">
-          <a className="underline" target="_blank" rel="noreferrer">
-            repa
-          </a>
-        </Link>
-      </p>
+      <Link href="mailto:petr@khome.cz">
+        <a className="w-[90vw]">
+          <IconMail className="inline mr-2" />
+          petr@khome.cz
+        </a>
+      </Link>
     </footer>
   );
 }
