21495380a8e3f2357defff5e2e8ff297ed27ec45
petrkucerak <petr@khome.cz>
2022-10-27 16:23:44 +0200

    Add a content on the card

--- components/map-toilets.js
+++ components/map-toilets.js
@@ -9,20 +9,44 @@ export default function MapToilets() {
     .then((res) => res.json())
     .then((toilets) => {
       for (let id = 0; id < toilets.length; id += 1) {
+        // prepare hte category string
+        let category = "<div>";
+        if (toilets[id].isClean) category += "<span>čisto</span>";
+        if (toilets[id].hasPaper) category += "<span>toaleťák</span>";
+        if (toilets[id].canLock) category += "<span>zamykatelné</span>";
+        if (toilets[id].hasWater) category += "<span>tekoucí voda</span>";
+        if (toilets[id].isFree) category += "<span>zdarma</span>";
+        for (let i = 0; i < toilets[id].bonusCategory.length; i += 1) {
+          category += `<span>${toilets[id].bonusCategory[i]}</span>`;
+        }
+        category += "</div>";
+
+        //prepare date string
+        let date = new Date(toilets[id].timeStamp);
+        console.log(date);
+        const dateString = `${date.getDate()}. ${
+          date.getMonth() + 1
+        }. ${date.getFullYear()} v ${date.getHours()}:${date.getMinutes()}`;
+
+        // prepare the output string
+        const string = `
+          <div class="text-lg">
+            <h2 class="text-2xl font-semibold">${toilets[id].placeName}</h2>
+            <span class="font-mono text-base">${toilets[id].latitude}; ${toilets[id].longtitude}</span>
+            <span>${toilets[id].toiletType}</span>
+            ${category}
+            <p>${toilets[id].wayDescription}<p>
+            <p>${toilets[id].comment}<p>
+            <p>${dateString} přidal ${toilets[id].nickName}<p>
+          </div>
+        `;
+
+        // render card
         const toiletPopup = L.popup({
           keepInView: true,
           closeButton: false,
           className: "",
-        }).setContent(`
-          <div class="text-lg">
-            <h2 class="text-2xl font-semibold">${toilets[id].placeName}</h2>
-            <span class="font-mono text-base">${toilets[id].latitude}; ${toilets[id].longtitude}</span>
-            <div>
-              ${toilets[id].bonusCategory}
-            </div>
-            <p>${toilets[id].comment}<p>
-          </div>
-        `);
+        }).setContent(string);
 
         L.marker([toilets[id].latitude, toilets[id].longtitude], {
           icon: toiletIcon,
