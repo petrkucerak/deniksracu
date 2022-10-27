9c76e4bf1fa624208ff9e9a121828968acd32bb7
petrkucerak <petr@khome.cz>
2022-10-27 23:27:09 +0200

    Add rank levels

--- pages/hall.js
+++ pages/hall.js
@@ -5,24 +5,48 @@ import SectionHeader from "../components/section-header";
 import SectionBody from "../components/section-body";
 import LocalTitle from "../components/local-title";
 import BackButton from "../components/back-button";
+import { getData2HallOfShits } from "../lib/api";
 
-export default function Hall() {
-
+export default function Hall({ nickNames }) {
   return (
     <>
       <Meta />
       <BackButton />
-      <h1 className="text-center text-4xl font-bold my-2">
-        Síň sráčů
-      </h1>
+      <h1 className="text-center text-4xl font-bold my-2">Síň sráčů</h1>
       <Section>
         <SectionHeader title="Sráči" />
         <SectionBody>
           <LocalTitle title="Legendy" subtitle="" />
-          
+          <ul>
+            {nickNames.map((person) => {
+              return (
+                <li key={person.nickName} className={person.class}>
+                  {person.title} <strong>{person.nickName}</strong>:{" "}
+                  {person.score} {person.extension}
+                </li>
+              );
+            })}
+          </ul>
+        </SectionBody>
+        <SectionHeader title="Ranky" />
+        <SectionBody>
+          <ul>
+            <li className="srac-zelenac">Sráč zelenáč: 1+ trůnů</li>
+            <li className="srac">Sráč: 5+ trůnů</li>
+            <li className="srac-mazak">Sráč mazák: 20+ trůnů</li>
+            <li className="velesrac">Velesráč: 50+ trůnů</li>
+          </ul>
         </SectionBody>
       </Section>
       <Footer />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const nickNames = getData2HallOfShits();
+
+  return {
+    props: { nickNames },
+  };
 }
