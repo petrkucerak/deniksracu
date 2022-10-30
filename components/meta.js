092a1e8f9274bcd946b19a87380a590ba2ad7578
Petr Kučera <petr@khome.cz>
2022-10-30 10:44:12 +0100

    Add a splashscreens (#112)
    * Add a splashscreens
    
    * Optimised images with calibre/image-actions
    
    * Add a correct splashscreen
    
    * Add a social image for preview
    
    * Optimised images with calibre/image-actions
    
    * Add snippt to support PWA appearance on iOS
    
    * Remove snippt to support PWA appearance on iOS
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/meta.js
+++ components/meta.js
@@ -7,6 +7,7 @@ export default function Meta() {
   const keywords =
     "Deník sráčů, záchod, toalety, WC, položit kabel, saulit, trůnění, sráč";
   const mainColor = `af8566ff`;
+  const socialImage = "https://deniksracu.cz/asstes/logoSquare.png";
   const url = "https://deniksracu.cz/";
   return (
     <Head>
@@ -41,12 +42,64 @@ export default function Meta() {
         href="favicon/safari-pinned-tab.svg"
         color="#5bbad5"
       />
+
+      <link
+        href="splashscreens/iphone5_splash.png"
+        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/iphone6_splash.png"
+        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/iphoneplus_splash.png"
+        media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/iphonex_splash.png"
+        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/iphonexr_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/iphonexsmax_splash.png"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/ipad_splash.png"
+        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/ipadpro1_splash.png"
+        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/ipadpro3_splash.png"
+        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+      <link
+        href="splashscreens/ipadpro2_splash.png"
+        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+        rel="apple-touch-startup-image"
+      />
+
       <meta name="msapplication-TileColor" content="#ffffff" />
 
       <meta name="application-name" content={title} />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="PWA App" />
+      <meta name="apple-mobile-web-app-title" content={title} />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta name="format-detection" content="telephone=no" />
@@ -58,13 +111,15 @@ export default function Meta() {
       <meta name="twitter:url" content={url} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:site" content="@deniksracu" />
+      <meta name="twitter:image" content={socialImage} />
+      <meta name="twitter:creator" content="@petrkucerak" />
 
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={title} />
       <meta property="og:url" content={url} />
+      <meta property="og:image" content={socialImage} />
 
       <meta name="robots" content="all" />
     </Head>
