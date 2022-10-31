5447d1a60384394669efbf756329d2e758c104f4
Petr Kučera <petr@khome.cz>
2022-10-31 20:04:26 +0100

    Update map-srac-location.js

--- components/map-srac-location.js
+++ components/map-srac-location.js
@@ -11,7 +11,7 @@ export default function SracLocation() {
         title: "Ukaž mi, kde jsem!",
       },
       locateOptions: {
-        enableHighAccuracy: true,
+        enableHighAccuracy: false,
       },
     })
     .addTo(map);
