caa07da0b6fdafe42de2ef40e538e78c2b501c3b
Petr Kucera <petr@khome.cz>
2022-11-09 16:37:13 +0100

    Prepare icons for status

--- components/toilet-types.js
+++ components/toilet-types.js
@@ -10,6 +10,13 @@ export const toiletTypes = [
 
 const color = "#af8566ff";
 
+export const clearIcon = ``;
+export const paperIcon = ``;
+export const kayIcon = ``;
+export const wahterIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-droplet-filled" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.8 11a6 6 0 1 0 10.396 0l-5.197 -8l-5.2 8z" /><path d="M12 3v17" /><path d="M12 12l3.544 -3.544" /><path d="M12 17.3l5.558 -5.558" /></svg>`;
+export const freeIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-credit-card-off" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="3" y1="3" x2="21" y2="21" /><path d="M9 5h9a3 3 0 0 1 3 3v8a3 3 0 0 1 -.128 .87" /><path d="M18.87 18.872a3 3 0 0 1 -.87 .128h-12a3 3 0 0 1 -3 -3v-8c0 -1.352 .894 -2.495 2.124 -2.87" /><line x1="3" y1="11" x2="11" y2="11" /><line x1="15" y1="11" x2="21" y2="11" /><line x1="7" y1="15" x2="7.01" y2="15" /><line x1="11" y1="15" x2="13" y2="15" /></svg>`;
+export const bonusIcon = ``;
+
 export const toiletIcons = [
   `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-glass-full" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
