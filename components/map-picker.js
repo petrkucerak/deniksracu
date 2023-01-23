36fa72668a75aa997c150f8ca77a85122e9f9a42
Petr Kucera <petr@khome.cz>
2023-01-23 11:42:44 +0100

    Add popUp with button to form

--- components/map-picker.js
+++ components/map-picker.js
@@ -8,7 +8,7 @@ export default function MapPicker() {
     "click",
     (e) => {
       console.log("Click!");
-      L.marker(e.latlng, { icon: poopIcon }).addTo(map);
+      L.marker(e.latlng, { icon: poopIcon }).addTo(map).bindPopup(poopPopUp);
     },
     this
   );
@@ -21,3 +21,38 @@ const poopIcon = L.icon({
   iconAnchor: [16, 16],
   popupAnchor: [0, 0],
 });
+
+const newToilteIntro = [
+  "Sralo se ti tu dobře?",
+  "Jaké tu bylo tůnění?",
+  "Doporučil bys to tu?",
+];
+
+const string = `
+<div class="text-lg px-4 py-5">
+  <div>${
+    newToilteIntro[Math.floor(Math.random() * newToilteIntro.length)]
+  }</div>
+  <a><button class="
+    text-sm
+    rounded-xl
+    bg-yellow-500
+    w-full
+    py-2 px-2
+    mt-1
+    text-white
+    font-bold
+    uppercase
+    hover:bg-yellow-600
+    duration-200"
+  >přidat nový trůn</button></a>
+</div>
+`;
+
+const poopPopUp = L.popup({
+  keepInView: true,
+  offset: L.point(0, 0),
+  autoPanPadding: L.point(15, 15),
+  closeButton: false,
+  className: "",
+}).setContent(string);
