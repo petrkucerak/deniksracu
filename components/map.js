ab659d0aad6fc825a1d28c994a5d19923f732413
Petr Kučera <petr@khome.cz>
2024-06-01 11:57:04 +0200

    Fix blurred map on phones (#637)
    * Setup image rendering
    
    * Render different size for phones and for computer
    
    * Switch size for larger devices

--- components/map.js
+++ components/map.js
@@ -5,9 +5,12 @@ import "leaflet/dist/leaflet.css";
 import SracLocation from "../components/map-srac-location";
 import MapToilets from "../components/map-toilets";
 import MapPicker from "./map-picker";
+import { isMobile } from "react-device-detect";
 
 export default function Map() {
-  const API_KEY="0pDMFK05UmFV-CqGrkNGlwvIIquKNNSwUaFxI2ZLXQo"
+  const API_KEY = "0pDMFK05UmFV-CqGrkNGlwvIIquKNNSwUaFxI2ZLXQo";
+  let tileSize = "";
+  isMobile ? (tileSize = "256@2x") : tileSize = "256";
   return (
     <MapContainer
       center={[50.08061, 14.4101822]}
@@ -16,7 +19,7 @@ export default function Map() {
       className="w-screen h-screen diseable-map-selection"
     >
       <TileLayer
-        url={`https://api.mapy.cz/v1/maptiles/outdoor/256/{z}/{x}/{y}?apikey=${API_KEY}`}
+        url={`https://api.mapy.cz/v1/maptiles/outdoor/${tileSize}/{z}/{x}/{y}?apikey=${API_KEY}`}
         attribution='<a href="https://api.mapy.cz/copyright" target="_blank" rel="noreferrer">&copy; Seznam.cz a.s. a další</a>'
       />
       <SracLocation />
@@ -26,7 +29,14 @@ export default function Map() {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.76.0/dist/L.Control.Locate.min.css"
       />
-      <a href="http://mapy.cz/" target="_blank" rel="noreferrer" className="absolute z-[1000] bottom-0"><img alt="Mapy.cz logo" src="https://api.mapy.cz/img/api/logo.svg"/></a>
+      <a
+        href="http://mapy.cz/"
+        target="_blank"
+        rel="noreferrer"
+        className="absolute z-[1000] bottom-0"
+      >
+        <img alt="Mapy.cz logo" src="https://api.mapy.cz/img/api/logo.svg" />
+      </a>
     </MapContainer>
   );
 }
