5860a4b3e2e429e223b0fd93d26e18e65628a2af
petrkucerak <petr@khome.cz>
2025-07-28 19:09:28 +0200

    feat: config backend

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
